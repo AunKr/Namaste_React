efab0ae607a291335f05d9e4a4cf6c7020be27a2
ArunKr <arunaj9891@gmail.com>
2023-05-21 19:04:39 +0530

    feat: Laying the foundation

--- Lession1/App.js
+++ Lession1/App.js
@@ -1,23 +1,23 @@
 import React from "react";
-import {render} from 'react-dom'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
-const parent = React.createElement('div', {id: 'parent'},[
-    React.createElement('div', {id: 'child1'}, [
-        React.createElement('h1', {}, 'Child 1 H1 tag'),  
-        React.createElement('h2', {}, 'Child 1 H2 tag'),  
-    ]),
-    React.createElement('div', {id: 'child2'}, [
-        React.createElement('h1', {}, 'Child 2 H1 tag'),  
-        React.createElement('h2', {}, 'Child 2 H2 tag'),  
-    ]),
-])
-console.log('parent :', parent);  //ReactElement ==>> JS Object
+const Title = () => {
+  return <div>Title</div>;
+};
 
-const jsxParent = <div id="parent">Praent Div</div>
-console.log('jsxParent :', jsxParent); //ReactElement ==>> JS Object, not a div tag
+const HeadingComponent = () => {
+  return (
+    <>
+      <div className="container1">
+        {Title()}
+        <Title />
+        <Title></Title>
+        <div>Heading Component</div>
+      </div>
+      <div className="container2">Heading 2</div>
+    </>
+  );
+};
 
-//JSX ==>> HTML like syntax which inturn is ReactElement not HTML in JS
-
-const root = createRoot(document.getElementById('root'));
-root.render(jsxParent);
+const root = createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
